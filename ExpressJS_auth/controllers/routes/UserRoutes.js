2423b97a8a2894c8415d8d122608e5df6960b905
Miłosz <bugiel.milosz@gmail.com>
2025-08-04 09:52:51 +0200

    auth and sleep backend

---
+++ ExpressJS_auth/controllers/routes/UserRoutes.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const userController=require('../UserController');
+
+const router=express.Router();
+
+
+router.get('/users', userController.getAllAccount);
+router.post('/users/create', userController.createAccount);
+router.post('/users/login', userController.login);
+router.get('/users/logout', userController.logout);
+
+router.post('/users/forgotPassword', userController.forgotPassword);
+router.post('/users/editAccount', userController.editAccount);
+router.post('/users/updatePassword', userController.updatePassword);
+router.post('/users/setPassword', userController.setPassword);
+
+module.exports=router;
---
+++ ExpressJS_sleep/constants/static.js
@@ -0,0 +1,6 @@
+export const shared = {
+    db_user:"esmovia",
+    db_password:"",
+    db_address:"localhost:1111",
+    key:""
+}
