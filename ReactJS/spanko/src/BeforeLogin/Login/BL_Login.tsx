27aa9b8275ccad2cdb97eff231a414887fd1d4ba
Miłosz <bugiel.milosz@gmail.com>
2025-08-05 12:50:38 +0200

    login connected to api

--- ReactJS/spanko/src/BeforeLogin/Login/BL_Login.tsx
+++ ReactJS/spanko/src/BeforeLogin/Login/BL_Login.tsx
@@ -1,12 +1,73 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import '../../AfterLogin/NavBar/C_NavBar.css'
 import './BL_Login.css'
+import { useForm, type SubmitHandler } from 'react-hook-form';
 
 
 
 function BL_Login() {
   const leftCloud = useRef<HTMLImageElement>(null);
   const rightCloud = useRef<HTMLImageElement>(null);
+
+  const [registerForm, setRegisterForm] = useState(false);
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const formLogin = useRef<HTMLFormElement>(null);
+
+  const subm = useRef<HTMLInputElement>(null);
+
+  const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<{username:string, password:string}>()
+    const onSubmit: SubmitHandler<{username:string, password:string}> = (data) => {
+        console.log(data);
+
+        const requestOptions:RequestInit = {
+            credentials: 'omit', 
+            method:'POST', 
+            headers: { 'Content-Type': 'application/json' }, 
+            body:JSON.stringify({ username: data.username, password:data.password })
+        };
+
+        if(registerForm){
+
+          requestOptions.body = JSON.stringify({ username: data.username, password:data.password, email:email, phone:phone });
+          fetch('https://172.24.3.142:3000/api/users/create', requestOptions)
+              .then(() => {
+  
+                  alert("successfully created account");
+              })
+              .catch(err => {
+                  console.log(err);    
+                  alert("an error occurred")      
+              });
+
+        }else{
+
+          fetch('https://172.24.3.142:3000/api/users/login', requestOptions)
+              .then(async (response) => {
+                  const jsonObj = await response.json();
+  
+                  if(jsonObj != "-1"){
+                    localStorage.setItem("token", jsonObj.token);
+                    login();
+                    alert("server response: "+jsonObj.token);
+                  }else{
+                    alert("invalid username or password");
+                  }
+              })
+              .catch(err => {
+                  console.log(err);                
+              });
+        }
+
+    }
+
+
 
   function login()
   {
@@ -24,10 +85,30 @@ function BL_Login() {
 
     
     setTimeout(()=>{
-      localStorage.setItem("token", "test");
       location.reload();
     }, 500);
   }
+
+  let changeForm;
+  let title;
+  let buttonTitle;
+  let registerInputs;
+  if(registerForm){
+    changeForm = <p style={{color:"#5986FF", textAlign:"right", marginRight: "25px", marginTop:"10px", cursor:"pointer"}} onClick={() => setRegisterForm(!registerForm)}>Log in</p>;
+    title = "Register";
+    buttonTitle = "Register";
+    registerInputs = 
+    <>
+      <input type="text" placeholder='Your e-mail address' name="email" id="email" onChange={(event)=>{ setEmail(event.target.value) }}/>
+      <input type="text" name="phone" placeholder='Your phone number' id="phone" onChange={(event)=>{ setPhone(event.target.value) }}/>
+    </>
+  }else{
+    changeForm = <p style={{color:"#5986FF", textAlign:"right", marginRight: "25px", marginTop:"10px", cursor:"pointer"}} onClick={() => setRegisterForm(!registerForm)}>Create account</p>;
+    title = "Log into";
+    buttonTitle = "Log in";
+    registerInputs = <></>
+  }
+
 
   return (
     <>
@@ -51,22 +132,51 @@ function BL_Login() {
               <div className='D_LoginPanel'>
                 <div style={{margin:"20px"}}>
                   <h1 style={{color:"#5986FF", fontSize:"4em", textAlign:"center"}}>Hello</h1>
-                  <p style={{color:"#5986FF", fontSize:"1em", textAlign:"center"}}>Login into your account</p>
+                  <p style={{color:"#5986FF", fontSize:"1em", textAlign:"center"}}>
+                    {title} your account
+                  </p>
                 </div>
                 
-                <div style={{margin:"20px"}}>
-                  <input type='text' style={{color:"#5986FF"}}/>
-                  <input type='text' style={{color:"#5986FF"}}/>
-                </div>
+                <form method='post' ref={formLogin} onSubmit={handleSubmit(onSubmit)} style={{margin:"20px"}}>
+                  
+                  <input id="username" placeholder='Username' type="text" {...register("username", 
+                      {
+                          required: "Username field is required",
+                      })} 
+                  />
+                  {
+                      errors.username &&
+                      <div>
+                          {errors.username.message}
+                      </div>
+                  }
+
+                  <input type='password' id="password" placeholder='Password' {...register("password", {required: "Message field is required"})}/>
+                  {
+                      errors.password &&
+                      <div>
+                          {errors.password.message}
+                      </div>
+                  }     
+
+                  {registerInputs}
+                  
+                  <input type="submit" ref={subm} value="" hidden />                       
+                </form>
 
                 <p style={{color:"#5986FF", textAlign:"right", marginRight: "25px", marginTop:"-20px"}}>Forgot the password?</p>
+                {changeForm}
 
                 <div style={{flex:"1"}}>
 
                 </div>
 
-                <div className='D_LoginButton' onClick={login}>
-                  <p>Log In</p>
+                <div className='D_LoginButton' onClick={() => subm.current?.click()}>
+                  <p>{buttonTitle}</p>
+                </div>
+
+                <div>
+                  
                 </div>
               </div>
 
--- ReactJS/spanko/src/BeforeLogin/Registration/BL_Registration.tsx
+++ ReactJS/spanko/src/BeforeLogin/Registration/BL_Registration.tsx
@@ -1,9 +1,165 @@
+import { useRef } from "react";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import './BL_Registration.css';
+
 function BL_Registration() {
+
+  const leftCloud = useRef<HTMLImageElement>(null);
+  const rightCloud = useRef<HTMLImageElement>(null);
+
+  const formLogin = useRef<HTMLFormElement>(null);
+
+  const subm = useRef<HTMLInputElement>(null);
+
+  const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<{username:string, password:string, email:string, phone:string}>()
+    const onSubmit: SubmitHandler<{username:string, password:string, email:string, phone:string}> = (data) => {
+        console.log(data);
+
+        const requestOptions:RequestInit = {
+            credentials: 'omit', 
+            method:'POST', 
+            headers: { 'Content-Type': 'application/json' }, 
+            body:JSON.stringify({ username: data.username, password:data.password, email: data.email, phone:data.phone })
+        };
+
+        fetch('https://172.24.3.142:3000/api/users/login', requestOptions)
+            .then(async (response) => {
+                const jsonObj = await response.json();
+
+                if(jsonObj != "-1"){
+                  localStorage.setItem("token", jsonObj.token);
+                  login();
+                  alert("server response: "+jsonObj.token);
+                }else{
+                  alert("invalid username or password");
+                }
+            })
+            .catch(err => {
+                console.log(err);                
+            });
+    }
+
+
+
+  function login()
+  {
+    if(leftCloud.current)
+    {
+      leftCloud.current.style.marginLeft = "-500px";
+      leftCloud.current.style.opacity = "0";
+    }
+
+    if(rightCloud.current)
+    {
+      rightCloud.current.style.marginLeft = "500px";
+      rightCloud.current.style.opacity = "0";
+    }
+
+    
+    setTimeout(()=>{
+      location.reload();
+    }, 500);
+  }
+
   return (
     <>
-      
+      <div className='C_NavBar_D_MainFrame'>
+        <div className='D_MainPage'>
+          <div className='D_MainInFrame'>
+            <div className='D_LogoSpace'>
+              <img src='./logo.png'/>
+            </div>
+
+            <div className='D_TopLogoBoxHolder'>
+              <div className='D_TopLogoBox'>
+
+              </div>
+            </div>
+
+              <div className='D_StartSpanko'>
+              <img src='./chmureklewy.png' className='D_CloudeLogin_01' ref={leftCloud}/>
+              <img src='./chmurekprawy.png' className='D_CloudeLogin_02' ref={rightCloud}/>
+
+              <div className='D_LoginPanel'>
+                <div style={{margin:"20px"}}>
+                  <h1 style={{color:"#5986FF", fontSize:"4em", textAlign:"center"}}>Hello</h1>
+                  <p style={{color:"#5986FF", fontSize:"1em", textAlign:"center"}}>Login into your account</p>
+                </div>
+                
+                <form ref={formLogin} onSubmit={handleSubmit(onSubmit)} style={{margin:"20px"}}>
+                  
+                  <input id="username" type="text" {...register("username", 
+                      {
+                          required: "Username field is required",
+                      })} 
+                  />
+                  {
+                      errors.username &&
+                      <div>
+                          {errors.username.message}
+                      </div>
+                  }
+
+                  <input id="message" {...register("password", {required: "Password field is required"})}/>
+                  {
+                      errors.password &&
+                      <div>
+                          {errors.password.message}
+                      </div>
+                  }     
+                  
+                  <input id="email" {...register("email", 
+                    {
+                        required: "E-mail field is required", 
+                        pattern: {
+                            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                            message: "Email is not valid"
+                        }
+                    }
+                  )}/>
+                  {
+                      errors.password &&
+                      <div>
+                          {errors.password.message}
+                      </div>
+                  }     
+                  
+                  <input id="phone" {...register("phone", {required: "Phone field is required"})}/>
+                  {
+                      errors.password &&
+                      <div>
+                          {errors.password.message}
+                      </div>
+                  }     
+                  <input type="submit" ref={subm} value="" hidden />                       
+                </form>
+
+                <p style={{color:"#5986FF", textAlign:"right", marginRight: "25px", marginTop:"-20px"}}>Forgot the password?</p>
+
+                <div style={{flex:"1"}}>
+
+                </div>
+
+                <div className='D_LoginButton' onClick={() => subm.current?.click()}>
+                  <p>Log In</p>
+                </div>
+              </div>
+
+              <div className='D_BottomBox'>
+
+              </div>
+              </div>
+          </div>
+        </div>
+      </div>
     </>
   )
+  
 }
 
 export default BL_Registration
