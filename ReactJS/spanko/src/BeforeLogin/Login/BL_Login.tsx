7671f4c68e45313d81d76ab31192f002772ec67a
MBernackiProgrammer <mbernacki06@gmail.com>
2025-08-05 09:15:36 +0200

    loginPanel

--- ReactJS/spanko/src/BeforeLogin/Login/BL_Login.tsx
+++ ReactJS/spanko/src/BeforeLogin/Login/BL_Login.tsx
@@ -1,6 +1,34 @@
+import { useRef } from 'react';
 import '../../AfterLogin/NavBar/C_NavBar.css'
+import './BL_Login.css'
+
+
 
 function BL_Login() {
+  const leftCloud = useRef<HTMLImageElement>(null);
+  const rightCloud = useRef<HTMLImageElement>(null);
+
+  function login()
+  {
+    if(leftCloud.current)
+    {
+      leftCloud.current.style.marginLeft = "-500px";
+      leftCloud.current.style.opacity = "0";
+    }
+
+    if(rightCloud.current)
+    {
+      rightCloud.current.style.marginLeft = "500px";
+      rightCloud.current.style.opacity = "0";
+    }
+
+    
+    setTimeout(()=>{
+      localStorage.setItem("token", "test");
+      location.reload();
+    }, 500);
+  }
+
   return (
     <>
       <div className='C_NavBar_D_MainFrame'>
@@ -17,7 +45,34 @@ function BL_Login() {
             </div>
 
              <div className='D_StartSpanko'>
-              
+              <img src='./chmureklewy.png' className='D_CloudeLogin_01' ref={leftCloud}/>
+              <img src='./chmurekprawy.png' className='D_CloudeLogin_02' ref={rightCloud}/>
+
+              <div className='D_LoginPanel'>
+                <div style={{margin:"20px"}}>
+                  <h1 style={{color:"#5986FF", fontSize:"4em", textAlign:"center"}}>Hello</h1>
+                  <p style={{color:"#5986FF", fontSize:"1em", textAlign:"center"}}>Login into your account</p>
+                </div>
+                
+                <div style={{margin:"20px"}}>
+                  <input type='text' style={{color:"#5986FF"}}/>
+                  <input type='text' style={{color:"#5986FF"}}/>
+                </div>
+
+                <p style={{color:"#5986FF", textAlign:"right", marginRight: "25px", marginTop:"-20px"}}>Forgot the password?</p>
+
+                <div style={{flex:"1"}}>
+
+                </div>
+
+                <div className='D_LoginButton' onClick={login}>
+                  <p>Log In</p>
+                </div>
+              </div>
+
+              <div className='D_BottomBox'>
+
+              </div>
              </div>
           </div>
         </div>
